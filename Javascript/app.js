5e0ec7b0b4f663157938078dd95bd68137a16b7f
Shaun <torianharris917@gmail.com>
2019-01-09 06:32:13 -0500

    added firebase

---
+++ Javascript/app.js
@@ -0,0 +1,44 @@
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyCg5_dTcBwqFe48vGQBt6CPIggXYvltWXc",
+    authDomain: "rps-online-52e02.firebaseapp.com",
+    databaseURL: "https://rps-online-52e02.firebaseio.com",
+    projectId: "rps-online-52e02",
+    storageBucket: "",
+    messagingSenderId: "294126086039"
+};
+
+firebase.initializeApp(config);
+
+let database = firebase.database();
+
+var connectionsRef = database.ref("/connections");
+var usersref = database.ref("/users");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+let connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+//console.log(connectionsRef.numChildren());
+connectedRef.on("value", function (snap) {
+
+    // If they are connected..
+    if (snap.val()) {
+        $("#status").html("Connected").addClass("text-success").removeClass("text-danger");
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+    }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snapshot) {
+
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+    $("#watchers").text(snapshot.numChildren());
+  });
