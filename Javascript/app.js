ab7ea6b5f71558523d7cbfc6b634b9262ceb9260
Shaun <torianharris917@gmail.com>
2019-01-10 17:42:35 -0500

    feat: room create/join/remove

--- Javascript/app.js
+++ Javascript/app.js
@@ -9,43 +9,69 @@ let config = {
 };
 
 firebase.initializeApp(config);
-
 let database = firebase.database();
 
-var connectionsRef = database.ref("/connections");
-var roomRef = database.ref("/rooms");
+let createdRoom = false;
+let name = prompt("Enter Name");
+let roomRef = database.ref("/rooms");
+//var connectionsRef = database.ref("/connections");
 
 // '.info/connected' is a special location provided by Firebase that is updated every time
 // the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-let connectedRef = database.ref(".info/connected");
+// let connectedRef = database.ref(".info/connected");
 
 // When the client's connection state changes...
 //console.log(connectionsRef.numChildren());
-connectedRef.on("value", function (snap) {
+// connectedRef.on("value", function (snap) {
+//     //$("#join-room").append($("<button>").text(snap.key));
+//     // If they are connected..
+//     if (snap.val()) {
+//         // Add user to the connections list.
+//         var con = connectionsRef.push(true);
 
-    // If they are connected..
-    if (snap.val()) {
-        // Add user to the connections list.
-        var con = connectionsRef.push(true);
+//         // Remove user from the connection list when they disconnect.
+//         con.onDisconnect().remove();
+//     }
+// });
 
-        // Remove user from the connection list when they disconnect.
-        con.onDisconnect().remove();
+roomRef.on("child_added", function (snapshot) {
+    $("#join-room").append($("<button>").text(snapshot.key).attr("room-id", snapshot.key));
+})
+
+// When first loaded or when the connections list changes...
+// connectionsRef.on("value", function (snapshot) {
+
+//     // Display the viewer count in the html.
+//     // The number of online users is the number of children in the connections list.
+//     $("#watchers").text(snapshot.numChildren());
+//     $("#status").html("Connected").addClass("text-success").removeClass("text-danger");
+// });
+
+$("#new-room").on("click", function () {
+    if(!createdRoom)
+    {
+        createdRoom = true;
+        let room = roomRef.push();
+        room.push({
+            userName: name
+        });
+        room.onDisconnect().remove();
     }
 });
 
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function (snapshot) {
-
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#watchers").text(snapshot.numChildren());
-    if (snapshot.numChildren() < 3)
-        $("#status").html("Connected").addClass("text-success").removeClass("text-danger");
-});
-
-let name = prompt("Enter Name")
-
-$("#new-room").on("click", function () {
-    let room = roomRef.push(name);
-});
+$("#join-room").on("click", "button", function () {
+    let roomID = $(this).attr("room-id");
+    let room = roomRef.child(roomID);
+    room.once("value", function (snapshot) {
+        if (snapshot.numChildren() < 2)
+        {
+            let user = room.push({
+                username: name
+            });
+            user.onDisconnect().remove();
+        }
+        else
+            alert("this room is full");
+    });
+})
